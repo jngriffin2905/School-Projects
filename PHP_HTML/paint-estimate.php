e920f77869bec2035518b43cf2960660399e31f7
jngriffin2905 <124809062+jngriffin2905@users.noreply.github.com>
2024-07-23 16:05:32 -0400

    Add files via upload

--- PHP_HTML/paint-estimate.php
+++ PHP_HTML/paint-estimate.php
@@ -2,10 +2,9 @@
 <!--Author:
 	Date:
 	File:	paint-estimate.php
-	Purpose:Chapter 5 Exercise
+	Purpose:Chapter 8 Exercise
 
 -->
-
 
 <html>
 <head>
@@ -17,38 +16,29 @@
 	<h1>King Painting: Paint Estimate</h1>
 
 	<?php
-	$dollarsPerGallon = 17.00;
-	$laborCost = 25.00;
+		$roomLength = $_POST['roomLength'];
+		$roomWidth = $_POST['roomWidth'];
+		$roomHeight = $_POST['roomHeight'];
+		$paint = $_POST['paint'];
+		$firstTime = $_POST['firstTime'];
 
-	$roomLength = $_POST['roomLength'];
-	$roomWidth = $_POST['room_Width'];
-	$roomHeight = $_POST['room_Height'];
-	
-	$areaLongWall = $roomLength * $roomHeight;
-	$areaWideWall = $roomWidth * $roomHeight;
-	$areaCeiling = $roomLength * $roomHeight;
-	$totalArea = $areaLongWall * 2 + $areaWideWall * 2 + $areaCeiling;
-	$numGallons = $totalArea/400;
-	$coveragePerGallon = 400;
-	$coveragePerHour = 200;
-	$hoursOfLabor = $totalArea/200;
-	$paintCost = $totalArea/$coveragePerGallon * $dollarsPerGallon ;
-	$laborCost = $totalArea/$coveragePerHour * $laborCost;
-	$totalCost = $paintCost + $laborCost;
-	$numGallons= ceil($numGallons/$coveragePerGallon);
-	$hoursOfLabor = ceil($totalArea/200);
-	
+		$longWallArea = $roomLength * $roomHeight;
+		$wideWallArea = $roomWidth * $roomHeight;
+		$ceilingArea = $roomLength * $roomWidth;
+		$totalArea = $longWallArea * 2 + $wideWallArea * 2 + $ceilingArea;
+		
+		$paintCost = ceil($totalArea / 400) * 15.00;
 
-	
-	print("<p>Your length is $roomLength feet. Your width is $roomWidth feet. Your height is $roomHeight feet.</p>");
-	print("<p>Your total area is $totalArea sq feet.</p>");
-	print("<p>Your total hours of labor are $hoursOfLabor.</p>");
-	print("<p>The number of gallons needed is $numGallons.</p>"); 
-	print("<p>Your paint cost is $$paintCost. Your labor cost is $$laborCost.</p>");
-	print("<p>Your total cost is $$totalCost.</p>");
-	
+		$laborCost = ceil($totalArea / 200) * 25.00;
+		$totalCost = $paintCost + $laborCost;
 
+		print("<p>Length of room: $roomLength<br>");
+		print("Width of room: $roomWidth<br>");
+		print("Height of room: $roomHeight</p>");
+		print("<p>Paint Cost: $".number_format($paintCost, 2)."<br>");
+		print("Labor Cost: $".number_format($laborCost, 2)."</p>");
 
+		print("<strong><p>Total Cost: $".number_format($totalCost,2)."</p></strong>");
 	?>
 
 </body>
